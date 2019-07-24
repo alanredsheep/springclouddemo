964c5d64c297989945869089b308b3304ebe1a01
alanredsheep <1103165118@qq.com>
2019-07-24 15:03:21 +0800

    remote config

--- configserver/src/main/java/com/redsheep/ConfigServerApplication.java
+++ configserver/src/main/java/com/redsheep/ConfigServerApplication.java
@@ -2,12 +2,10 @@ package com.redsheep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-/**
- * @author redsheep
- * @date 2019/7/24
- */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
