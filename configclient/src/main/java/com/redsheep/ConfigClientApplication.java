4492ed8749e3e1ba1ec85fccfee252a8590b240a
alanredsheep <1103165118@qq.com>
2019-07-24 14:32:42 +0800

    remote config

---
+++ configclient/src/main/java/com/redsheep/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.redsheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author redsheep
+ * @date 2019/7/24
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
